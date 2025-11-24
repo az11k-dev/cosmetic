19ce6e4106f5b41ef6ed5ae32c4997d58c49ebd8
az11k-dev <bazaclash55@gmail.com>
2025-11-24 09:27:30 +0500

    build-fix

--- src/pages/main/track-order-2/page.tsx
+++ src/pages/main/track-order-2/page.tsx
@@ -1,14 +1,9 @@
 import Breadcrumb from '@/components/breadcrumb/Breadcrumb'
-import TrackOrderTwo from '@/components/track-order/TrackOrderTwo'
 
 const page = () => {
     return (
         <>
-
-
-            <Breadcrumb title={"Track Order"} />
-            <TrackOrderTwo />
-
+            <Breadcrumb title={"Track Order"}/>
         </>
     )
 }
